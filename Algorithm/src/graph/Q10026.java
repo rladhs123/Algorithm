0aa63153137376e45ab4710bb79f4eea590a6d17
rladhs123 <151420186+rladhs123@users.noreply.github.com>
2025-02-20 18:10:23 +0900

    Create Q10026.java

---
+++ Algorithm/src/graph/Q10026.java
@@ -0,0 +1,85 @@
+package graph;
+
+import java.util.Scanner;
+
+/**
+ * 적록색약
+ *
+ * 적록색약은 빨간색과 초록색의 차이를 거의 느끼지 못한다. 따라서, 적록색약인 사람이 보는 그림은 아닌 사람이 보는 그림과는 좀 다를 수 있다.
+ *
+ * 크기가 N×N인 그리드의 각 칸에 R(빨강), G(초록), B(파랑) 중 하나를 색칠한 그림이 있다.
+ * 그림은 몇 개의 구역으로 나뉘어져 있는데, 구역은 같은 색으로 이루어져 있다.
+ * 또, 같은 색상이 상하좌우로 인접해 있는 경우에 두 글자는 같은 구역에 속한다.
+ * 그림이 입력으로 주어졌을 때,
+ * 적록색약인 사람이 봤을 때와 아닌 사람이 봤을 때 구역의 수를 구하는 프로그램을 작성하시오.
+ */
+public class Q10026 {
+
+    static int N;
+    static char[][] map; //일반인이 보는 그림
+    static boolean[][] isVisited;
+    static int[] dy = {-1, 1, 0, 0};
+    static int[] dx = {0, 0, -1, 1};
+    static int count;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        map = new char[N][N];
+        isVisited = new boolean[N][N];
+
+        for (int i = 0; i < N; i++) {
+            String input = sc.next();
+            for (int j = 0; j < N; j++) {
+                map[i][j] = input.charAt(j);
+            }
+        }
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (!isVisited[i][j]) {
+                    dfs(i, j);
+                    count++;
+                }
+            }
+        }
+        System.out.print(count + " ");
+
+        count = 0;
+        isVisited = new boolean[N][N];
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (map[i][j] == 'G') {
+                    map[i][j] = 'R';
+                }
+            }
+        }
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (!isVisited[i][j]) {
+                    dfs(i, j);
+                    count++;
+                }
+            }
+        }
+        System.out.print(count);
+    }
+
+    static void dfs(int y, int x) {
+        char currChar = map[y][x];
+        isVisited[y][x] = true;
+
+        for (int i = 0; i < 4; i++) {
+            int nextY = y + dy[i];
+            int nextX = x + dx[i];
+
+            if (nextY >= 0 && nextX >= 0 && nextY < N && nextX < N && !isVisited[nextY][nextX]) {
+                if (currChar == map[nextY][nextX]) {
+                    dfs(nextY, nextX);
+                }
+            }
+        }
+    }
+}
