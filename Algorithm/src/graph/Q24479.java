ef4dba8a42e81d8c094d1ceff8f0d85260d16bc7
rladhs123 <151420186+rladhs123@users.noreply.github.com>
2025-03-14 17:13:23 +0900

    Create Q24479.java

---
+++ Algorithm/src/graph/Q24479.java
@@ -0,0 +1,60 @@
+package graph;
+
+import java.util.*;
+
+/**
+ * 깊이 우선 탐색(dfs)
+ */
+public class Q24479 {
+
+    static int N, M, R;
+    static List<Integer>[] graph;
+    static int[] visited;
+    static int count = 1;
+    static StringBuilder sb = new StringBuilder();
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        M = sc.nextInt();
+        R = sc.nextInt();
+        graph = new ArrayList[N + 1];
+        visited = new int[N + 1];
+
+        for (int i = 1; i < graph.length; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        for (int i = 0; i < M; i++) {
+            int u = sc.nextInt();
+            int v = sc.nextInt();
+
+            graph[u].add(v);
+            graph[v].add(u);
+        }
+
+        for (int i = 1; i < graph.length; i++) {
+            Collections.sort(graph[i]);
+        }
+
+        dfs(R);
+
+        for (int i = 1; i < visited.length; i++) {
+            sb.append(visited[i]).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+
+    static void dfs(int node) {
+        visited[node] = count;
+
+        for (Integer nextNode : graph[node]) {
+            if (visited[nextNode] == 0) {
+                count++;
+                dfs(nextNode);
+            }
+        }
+    }
+}
