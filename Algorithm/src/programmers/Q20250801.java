69ecbdb19cc22780f706f65309641a88b9d0f8ca
rladhs123 <151420186+rladhs123@users.noreply.github.com>
2025-08-01 12:31:25 +0900

    Create Q20250801.java

---
+++ Algorithm/src/programmers/Q20250801.java
@@ -0,0 +1,22 @@
+package programmers;
+
+public class Q20250801 {
+
+    static int[][] solution(int[][] arr1, int[][] arr2) {
+        int[][] answer = new int[arr1.length][arr2[0].length];
+
+        for (int i = 0; i < answer.length; i++) {
+            for (int j = 0; j < answer[i].length; j++) {
+                for (int k = 0; k < arr1[i].length; k++) {
+                    answer[i][j] += arr1[i][k] * arr2[k][j];
+                }
+            }
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        solution(new int[][]{{1, 4}, {3, 2}, {4, 1}}, new int[][]{{3, 3}, {3, 3}});
+    }
+}
