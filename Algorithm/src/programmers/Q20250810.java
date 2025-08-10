b66b99ccbde685dcbca417377876e132ddd263ad
rladhs123 <151420186+rladhs123@users.noreply.github.com>
2025-08-10 17:00:03 +0900

    Create Q20250810.java

---
+++ Algorithm/src/programmers/Q20250810.java
@@ -0,0 +1,26 @@
+package programmers;
+
+import java.util.Stack;
+
+public class Q20250810 {
+
+    public int[] solution(int []arr) {
+        int[] answer = {};
+        Stack<Integer> stack = new Stack<>();
+        stack.push(arr[0]);
+
+        for (int i = 1; i < arr.length; i++) {
+            if (stack.peek() != arr[i]) {
+                stack.push(arr[i]);
+            }
+        }
+
+        answer = new int[stack.size()];
+
+        for (int i = answer.length - 1; i >= 0; i--) {
+            answer[i] = stack.pop();
+        }
+
+        return answer;
+    }
+}
