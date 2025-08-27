6558be1f9e8cebf6bd1b376805cb8e7827a2e4b5
rladhs123 <151420186+rladhs123@users.noreply.github.com>
2025-08-27 14:00:58 +0900

    Create Q20250827.java

---
+++ Algorithm/src/programmers/Q20250827.java
@@ -0,0 +1,21 @@
+package programmers;
+
+public class Q20250827 {
+
+    public boolean solution(int x) {
+        boolean answer = true;
+        int sum = 0;
+        int n = x;
+
+        while (x != 0) {
+            sum += x % 10;
+            x /= 10;
+        }
+
+        if (n % sum != 0) {
+            answer = false;
+        }
+
+        return answer;
+    }
+}
