3f1c5a3b3239ee69dc0ed950b8a479b8f5f626ab
rladhs123 <151420186+rladhs123@users.noreply.github.com>
2025-08-29 10:46:56 +0900

    Create Q20250829.java

---
+++ Algorithm/src/programmers/Q20250829.java
@@ -0,0 +1,14 @@
+package programmers;
+
+public class Q20250829 {
+
+    public int solution(int[] a, int[] b) {
+        int answer = 0;
+
+        for (int i = 0; i < a.length; i++) {
+            answer += a[i] * b[i];
+        }
+
+        return answer;
+    }
+}
