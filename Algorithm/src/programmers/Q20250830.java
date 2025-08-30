af5f3224e8857f8d3ae30e0442674b99b1122298
rladhs123 <151420186+rladhs123@users.noreply.github.com>
2025-08-30 14:37:33 +0900

    Create Q20250830.java

---
+++ Algorithm/src/programmers/Q20250830.java
@@ -0,0 +1,21 @@
+package programmers;
+
+public class Q20250830 {
+
+    public String solution(int n) {
+        String answer = "";
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < n; i++) {
+            if (i % 2 == 0) {
+                sb.append("수");
+            } else {
+                sb.append("박");
+            }
+        }
+
+        answer = sb.toString();
+
+        return answer;
+    }
+}
