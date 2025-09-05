05b2d44a247811f2acdb48aa8069a895ea0aaf40
rladhs123 <151420186+rladhs123@users.noreply.github.com>
2025-09-05 13:46:02 +0900

    Create Q20250905.java

---
+++ Algorithm/src/programmers/Q20250905.java
@@ -0,0 +1,22 @@
+package programmers;
+
+public class Q20250905 {
+
+    public String solution(String s, int n) {
+        String answer = "";
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+
+            if (Character.isLowerCase(c)) {
+                c = (char) ((c - 'a' + n) % 26 + 'a');
+            } else if (Character.isUpperCase(c)) {
+                c = (char) ((c - 'A' + n) % 26 + 'A');
+            }
+
+            answer += c;
+        }
+
+        return answer;
+    }
+}
