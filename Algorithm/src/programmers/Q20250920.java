30ef8cca3708280cf7b1b850394818f0663a0607
rladhs123 <151420186+rladhs123@users.noreply.github.com>
2025-09-20 21:21:45 +0900

    Create Q20250920.java

---
+++ Algorithm/src/programmers/Q20250920.java
@@ -0,0 +1,28 @@
+package programmers;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Q20250920 {
+
+    public String[] solution(String[] players, String[] callings) {
+        String[] answer = {};
+        Map<String, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < players.length; i++) {
+            map.put(players[i], i);
+        }
+
+        for (String s : callings) {
+            int nextIndex = map.get(s) - 1;
+            String front = players[nextIndex];
+
+            players[nextIndex] = s;
+            players[map.get(s)] = front;
+            
+            map.put(s, nextIndex);
+            map.put(front, nextIndex + 1);
+        }
+        return players;
+    }
+}
