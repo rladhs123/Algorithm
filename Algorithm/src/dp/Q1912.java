fc6871e57887c9c7b93e5694ed62ffb3e78e7ca7
rladhs123 <151420186+rladhs123@users.noreply.github.com>
2025-10-03 21:54:43 +0900

    Create Q1912.java

---
+++ Algorithm/src/dp/Q1912.java
@@ -0,0 +1,34 @@
+package dp;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ * 연속합
+ *
+ * n개의 정수로 이루어진 임의의 수열이 주어진다.
+ * 우리는 이 중 연속된 몇 개의 수를 선택해서 구할 수 있는 합 중 가장 큰 합을 구하려고 한다. 단, 수는 한 개 이상 선택해야 한다.
+ * 예를 들어서 10, -4, 3, 1, 5, 6, -35, 12, 21, -1 이라는 수열이 주어졌다고 하자. 여기서 정답은 12+21인 33이 정답이 된다.
+ */
+public class Q1912 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int[] array = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            array[i] = sc.nextInt();
+        }
+
+        int[] dp = new int[N];
+        dp[0] = array[0];
+
+        for (int i = 1; i < N; i++) {
+            dp[i] = Math.max(dp[i - 1] + array[i], array[i]);
+        }
+
+        Arrays.sort(dp);
+        System.out.println(dp[N - 1]);
+    }
+}
