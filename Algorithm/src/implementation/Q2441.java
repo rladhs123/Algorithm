431ff729c0573fce4519bdc31d294b8c75bf13dd
rladhs123 <151420186+rladhs123@users.noreply.github.com>
2025-10-08 20:24:55 +0900

    Create Q2441.java

---
+++ Algorithm/src/implementation/Q2441.java
@@ -0,0 +1,25 @@
+package implementation;
+
+import java.util.Scanner;
+
+public class Q2441 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                sb.append(' ');
+            }
+
+            for (int j = i; j < n; j++) {
+                sb.append('*');
+            }
+            sb.append('\n');
+        }
+
+        System.out.println(sb);
+    }
+}
