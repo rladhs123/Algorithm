287acc624092d72e46804a5074fde8ee712d969e
rladhs123 <151420186+rladhs123@users.noreply.github.com>
2026-01-11 13:30:17 +0900

    Create Q1085.java

---
+++ Algorithm/src/implementation/Q1085.java
@@ -0,0 +1,20 @@
+package implementation;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Q1085 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int x = Integer.parseInt(st.nextToken());
+        int y = Integer.parseInt(st.nextToken());
+        int w = Integer.parseInt(st.nextToken());
+        int h = Integer.parseInt(st.nextToken());
+
+        System.out.println(Math.min(Math.min(y, h - y), Math.min(x, w - x)));
+    }
+}
