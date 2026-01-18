c6c0dc90266276959c4b3ac16aa6060fcb9847ed
rladhs123 <151420186+rladhs123@users.noreply.github.com>
2026-01-18 17:54:13 +0900

    Create Q9086.java

---
+++ Algorithm/src/string/Q9086.java
@@ -0,0 +1,21 @@
+package string;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Q9086 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < n; i++) {
+            String s = br.readLine();
+            sb.append(s.charAt(0)).append(s.charAt(s.length() - 1)).append('\n');
+        }
+
+        System.out.print(sb);
+    }
+}
