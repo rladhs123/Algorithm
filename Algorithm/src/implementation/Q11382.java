72c728f7234c7f3c87107480dc2983df04b00a0a
rladhs123 <151420186+rladhs123@users.noreply.github.com>
2026-01-19 09:33:47 +0900

    Create Q11382.java

---
+++ Algorithm/src/implementation/Q11382.java
@@ -0,0 +1,21 @@
+package implementation;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Q11382 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        long sum = 0;
+
+        for (int i = 0; i < 3; i++) {
+            sum += Long.parseLong(st.nextToken());
+        }
+
+        System.out.println(sum);
+    }
+}
