6700e77375b964b71c847bda022bd1d19d599a3c
rladhs123 <151420186+rladhs123@users.noreply.github.com>
2026-01-20 16:59:14 +0900

    Create Q8393.java

---
+++ Algorithm/src/implementation/Q8393.java
@@ -0,0 +1,14 @@
+package implementation;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Q8393 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        System.out.println(n * (1 + n) / 2);
+    }
+}
