d477cbd5be2482e2456dc46bc5be5bb20331b6f5
rladhs123 <151420186+rladhs123@users.noreply.github.com>
2026-01-30 14:35:52 +0900

    Create Q2440.java

---
+++ Algorithm/src/implementation/Q2440.java
@@ -0,0 +1,23 @@
+package implementation;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Q2440 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = n; i >= 0; i--) {
+            for (int j = 0; j < i; j++) {
+                sb.append('*');
+            }
+            sb.append('\n');
+        }
+
+        System.out.print(sb);
+    }
+}
