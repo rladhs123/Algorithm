a2f382152a9dbef80cee42c557860186dd0d420f
rladhs123 <151420186+rladhs123@users.noreply.github.com>
2026-02-01 21:06:32 +0900

    Create Q2558.java

---
+++ Algorithm/src/implementation/Q2558.java
@@ -0,0 +1,16 @@
+package implementation;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Q2558 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(br.readLine());
+        int b = Integer.parseInt(br.readLine());
+
+        System.out.println(a + b);
+    }
+}
