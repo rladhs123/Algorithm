322ba2db882bc96aae609b80128a42dfe7655eb8
rladhs123 <151420186+rladhs123@users.noreply.github.com>
2026-02-06 14:37:27 +0900

    Create Q2903.java

---
+++ Algorithm/src/mathematics/Q2903.java
@@ -0,0 +1,15 @@
+package mathematics;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Q2903 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        System.out.println((int)Math.pow((Math.pow(2, n) + 1),2));
+    }
+}
