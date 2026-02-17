9ec86a93bc2f1a70d8ec68743866402c6ed747a8
rladhs123 <151420186+rladhs123@users.noreply.github.com>
2026-02-17 11:14:20 +0900

    Create Q14215.java

---
+++ Algorithm/src/implementation/Q14215.java
@@ -0,0 +1,31 @@
+package implementation;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Q14215 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] arr = new int[3];
+
+        for (int i = 0; i < 3; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+        int sum = arr[0] + arr[1];
+
+        if (sum <= arr[2]) {
+            sum += sum - 1;
+        } else {
+            sum += arr[2];
+        }
+
+        System.out.println(sum);
+    }
+}
