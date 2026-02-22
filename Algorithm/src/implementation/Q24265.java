82fc8f86cf67afa5bd887b1f507d3541db043f57
rladhs123 <151420186+rladhs123@users.noreply.github.com>
2026-02-22 16:06:53 +0900

    Create Q24265.java

---
+++ Algorithm/src/implementation/Q24265.java
@@ -0,0 +1,15 @@
+package implementation;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Q24265 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        long n = Long.parseLong(br.readLine());
+
+        System.out.println((n * (n - 1)) / 2 + "\n" + 2);
+    }
+}
