7938a4104cf6300090f332ee42b3245063098569
rladhs123 <151420186+rladhs123@users.noreply.github.com>
2026-02-24 17:10:35 +0900

    Create Q24267.java

---
+++ Algorithm/src/implementation/Q24267.java
@@ -0,0 +1,15 @@
+package implementation;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Q24267 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        long n = Long.parseLong(br.readLine());
+
+        System.out.println((n * (n - 1) * (n - 2) / 6) + "\n" + 3);
+    }
+}
