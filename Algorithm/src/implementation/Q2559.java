5d4525443bfc079ceace6d95915935fda2e5fb16
rladhs123 <151420186+rladhs123@users.noreply.github.com>
2026-03-16 15:39:27 +0900

    Create Q2559.java

---
+++ Algorithm/src/implementation/Q2559.java
@@ -0,0 +1,40 @@
+package implementation;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Q2559 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        int[] arr = new int[n];
+        st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int sum = 0;
+
+        for (int i = 0; i < k; i++) {
+            sum += arr[i];
+        }
+
+        int max = sum;
+
+        for (int i = k; i < n; i++) {
+            sum = sum - arr[i - k] + arr[i];
+
+            if (sum > max) {
+                max = sum;
+            }
+        }
+
+        System.out.println(max);
+    }
+}
