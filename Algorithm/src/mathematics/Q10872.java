5a4a4e522ed8e40f3544a92b3d54b1d4231130c2
rladhs123 <151420186+rladhs123@users.noreply.github.com>
2026-03-20 14:55:47 +0900

    Create Q10872.java

---
+++ Algorithm/src/mathematics/Q10872.java
@@ -0,0 +1,20 @@
+package mathematics;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Q10872 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int x = 1;
+
+        for (int i = 1; i <= n; i++) {
+            x *= i;
+        }
+
+        System.out.println(x);
+    }
+}
