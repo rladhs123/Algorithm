af6c923da94fa9bb66dbad4a41b32f8dd1392c8d
rladhs123 <151420186+rladhs123@users.noreply.github.com>
2026-03-30 15:11:58 +0900

    Create Q2455.java

---
+++ Algorithm/src/implementation/Q2455.java
@@ -0,0 +1,30 @@
+package implementation;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Q2455 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int max = 0;
+        int current = 0;
+
+        for (int i = 0; i < 4; i++) {
+            st = new StringTokenizer(br.readLine());
+            int out = Integer.parseInt(st.nextToken());
+            int in = Integer.parseInt(st.nextToken());
+
+            current = current + in - out;
+
+            if (current > max) {
+                max = current;
+            }
+        }
+
+        System.out.println(max);
+    }
+}
