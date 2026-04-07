cf8302dee8df09115fa1ad1b3847a02f99255dc4
rladhs123 <151420186+rladhs123@users.noreply.github.com>
2026-04-07 18:13:13 +0900

    Create Q24883.java

---
+++ Algorithm/src/implementation/Q24883.java
@@ -0,0 +1,19 @@
+package implementation;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Q24883 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+
+        if (s.equals("N") || s.equals("n")) {
+            System.out.println("Naver D2");
+        } else {
+            System.out.println("Naver Whale");
+        }
+    }
+}
