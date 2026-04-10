fe6fddd1c80bca7d5eb33ceea7f2344035e9a030
rladhs123 <151420186+rladhs123@users.noreply.github.com>
2026-04-10 18:51:25 +0900

    Create Q10156.java
    day 425

---
+++ Algorithm/src/mathematics/Q10156.java
@@ -0,0 +1,19 @@
+package mathematics;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Q10156 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int k = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        System.out.println(Math.max(0, k * n - m));
+    }
+}
