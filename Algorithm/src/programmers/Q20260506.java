37dd7f91360e2a05f166a085315c488fa2bb88e1
rladhs123 <151420186+rladhs123@users.noreply.github.com>
2026-05-06 14:19:57 +0900

    Create Q20260506.java
    day 451

---
+++ Algorithm/src/programmers/Q20260506.java
@@ -0,0 +1,19 @@
+package programmers;
+
+public class Q20260506 {
+
+    public int solution(int order) {
+        int answer = 0;
+        String s = Integer.toString(order);
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+
+            if (c == '3' || c == '6' || c == '9') {
+                answer++;
+            }
+        }
+
+        return answer;
+    }
+}
