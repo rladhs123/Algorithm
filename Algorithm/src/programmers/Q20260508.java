700030cd5a039fe5f5e352631515becd83f600f8
rladhs123 <151420186+rladhs123@users.noreply.github.com>
2026-05-08 14:21:54 +0900

    Create Q20260508.java
    day 453

---
+++ Algorithm/src/programmers/Q20260508.java
@@ -0,0 +1,14 @@
+package programmers;
+
+public class Q20260508 {
+
+    public int[] solution(int[] numbers) {
+        int[] answer = new int[numbers.length];
+
+        for (int i = 0; i < numbers.length; i++) {
+            answer[i] = numbers[i] * 2;
+        }
+
+        return answer;
+    }
+}
