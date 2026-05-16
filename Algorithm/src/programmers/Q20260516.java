01d26feffd8136d3a6408351e5b454cf484d3731
rladhs123 <151420186+rladhs123@users.noreply.github.com>
2026-05-16 11:14:39 +0900

    Create Q20260516.java
    day 461

---
+++ Algorithm/src/programmers/Q20260516.java
@@ -0,0 +1,21 @@
+package programmers;
+
+public class Q20260516 {
+
+    public String solution(String my_string, int n) {
+        String answer = "";
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < my_string.length(); i++) {
+            char c = my_string.charAt(i);
+
+            for (int j = 0; j < n; j++) {
+                sb.append(c);
+            }
+        }
+
+        answer = sb.toString();
+
+        return answer;
+    }
+}
