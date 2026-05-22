6464bfefb1376582d30ab96aa112a48817cf0e8e
rladhs123 <151420186+rladhs123@users.noreply.github.com>
2026-05-22 19:06:42 +0900

    Create Q20260522.java
    day 467

---
+++ Algorithm/src/programmers/Q20260522.java
@@ -0,0 +1,17 @@
+package programmers;
+
+public class Q20260522 {
+
+    public String solution(String my_string) {
+        String answer = "";
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = my_string.length() - 1; i >= 0; i--) {
+            sb.append(my_string.charAt(i));
+        }
+
+        answer = sb.toString();
+
+        return answer;
+    }
+}
