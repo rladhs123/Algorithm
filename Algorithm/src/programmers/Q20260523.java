9509fdf6f406a59d696f4680d2910e217cd761d5
rladhs123 <151420186+rladhs123@users.noreply.github.com>
2026-05-23 19:37:32 +0900

    Create Q20260523.java
    day 468

---
+++ Algorithm/src/programmers/Q20260523.java
@@ -0,0 +1,9 @@
+package programmers;
+
+public class Q20260523 {
+
+    public int solution(int n, int k) {
+        int answer = 12000 * n + k * 2000 - (n / 10) * 2000;
+        return answer;
+    }
+}
