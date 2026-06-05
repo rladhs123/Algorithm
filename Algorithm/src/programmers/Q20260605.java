00885179ab002e73ea64c714362fb3afc981b58f
rladhs123 <151420186+rladhs123@users.noreply.github.com>
2026-06-05 19:25:41 +0900

    Create Q20260605.java
    day 431

---
+++ Algorithm/src/programmers/Q20260605.java
@@ -0,0 +1,20 @@
+package programmers;
+
+public class Q20260605 {
+
+    public int[] solution(String my_string) {
+        int[] answer = new int[52];
+
+        for (int i = 0; i < my_string.length(); i++) {
+            char c = my_string.charAt(i);
+
+            if (c >= 'A' && c <= 'Z') {
+                answer[c - 'A']++;
+            } else {
+                answer[26 + c - 'a']++;
+            }
+        }
+
+        return answer;
+    }
+}
