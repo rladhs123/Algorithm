059cbf27956ed422c380306122db92d18735ae63
rladhs123 <151420186+rladhs123@users.noreply.github.com>
2026-06-16 19:30:54 +0900

    Create Q20260616.java
    day 442

---
+++ Algorithm/src/programmers/Q20260616.java
@@ -0,0 +1,16 @@
+package programmers;
+
+public class Q20260616 {
+
+    public int solution(String str1, String str2) {
+        int answer = 0;
+
+        if (str1.contains(str2)) {
+            answer = 1;
+        } else {
+            answer = 2;
+        }
+
+        return answer;
+    }
+}
