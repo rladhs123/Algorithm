04d40d5dcecad64846a79ce7b14a00b33556849a
rladhs123 <151420186+rladhs123@users.noreply.github.com>
2026-06-20 16:31:59 +0900

    Create Q20260620.java
    day 446

---
+++ Algorithm/src/programmers/Q20260620.java
@@ -0,0 +1,14 @@
+package programmers;
+
+public class Q20260620 {
+
+    public int solution(int n, int t) {
+        int answer = n;
+
+        for (int i = 1; i <= t; i++) {
+            answer *= 2;
+        }
+
+        return answer;
+    }
+}
