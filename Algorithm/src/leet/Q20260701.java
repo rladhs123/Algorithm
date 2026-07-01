85c0341820e9429a6f22d1dd9a7babc5b1a282c7
rladhs123 <151420186+rladhs123@users.noreply.github.com>
2026-07-01 16:16:43 +0900

    solve Q20260701

--- Algorithm/src/leet/Q20260701.java
+++ Algorithm/src/leet/Q20260701.java
@@ -1,4 +1,28 @@
 package leet;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Q20260701 {
+
+    public List<String> buildArray(int[] target, int n) {
+        List<String> result = new ArrayList<>();
+        int index = 0;
+
+        for (int i = 1; i <= n; i++) {
+            result.add("Push");
+
+            if (target[index] == i) {
+                index++;
+            } else {
+                result.add("Pop");
+            }
+
+            if (target.length == index) {
+                break;
+            }
+        }
+
+        return result;
+    }
 }
