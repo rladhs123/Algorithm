967de10fa42f48a2022de9df973d585a8711d9ff
rladhs123 <151420186+rladhs123@users.noreply.github.com>
2026-07-01 15:49:25 +0900

    solve Q20260630

--- Algorithm/src/leet/Q20260630.java
+++ Algorithm/src/leet/Q20260630.java
@@ -1,4 +1,25 @@
 package leet;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Q20260630 {
+
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+        List<Integer> list = new ArrayList<>();
+        boolean[] check = new boolean[nums.length + 1];
+        int size = nums.length;
+
+        for (int i = 0; i < size; i++) {
+            check[nums[i]] = true;
+        }
+
+        for (int i = 1; i <= size; i++) {
+            if (!check[i]) {
+                list.add(i);
+            }
+        }
+
+        return list;
+    }
 }
