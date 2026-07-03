dc0f8482e33410a2d586d7a540c8b6596cd2365e
rladhs123 <151420186+rladhs123@users.noreply.github.com>
2026-07-03 09:08:25 +0900

    day 459

---
+++ Algorithm/src/leet/Q20260703.java
@@ -0,0 +1,35 @@
+package leet;
+
+import java.util.List;
+import java.util.Stack;
+
+public class Q20260703 {
+
+    public int[] exclusiveTime(int n, List<String> logs) {
+        int[] result = new int[n];
+        Stack<Integer> stack = new Stack<>();
+        int prevTime = 0;
+
+        for (String log : logs) {
+            String[] arr = log.split(":");
+
+            int id = Integer.parseInt(arr[0]);
+            String type = arr[1];
+            int time = Integer.parseInt(arr[2]);
+
+            if (type.equals("start")) {
+                if (!stack.isEmpty()) {
+                    result[stack.peek()] += time - prevTime;
+                }
+
+                stack.push(id);
+                prevTime = time;
+            } else {
+                result[stack.pop()] += time - prevTime + 1;
+                prevTime = time + 1;
+            }
+        }
+
+        return result;
+    }
+}
