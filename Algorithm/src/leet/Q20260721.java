a6f5f81d16a243bd2c18118cbad62da82f07d624
rladhs123 <151420186+rladhs123@users.noreply.github.com>
2026-07-21 16:28:09 +0900

    day 477

---
+++ Algorithm/src/leet/Q20260721.java
@@ -0,0 +1,16 @@
+package leet;
+
+public class Q20260721 {
+
+    public int sumOfMultiples(int n) {
+        int result = 0;
+
+        for (int i = 1; i <= n; i++) {
+            if (i % 3 == 0 || i % 5 == 0 || i % 7 == 0) {
+                result += i;
+            }
+        }
+
+        return result;
+    }
+}
