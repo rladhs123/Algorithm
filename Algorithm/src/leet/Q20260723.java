348c310602687828cddd848dffd158e169fefdc3
rladhs123 <151420186+rladhs123@users.noreply.github.com>
2026-07-23 18:13:32 +0900

    day 479

--- Algorithm/src/leet/Q20260723.java
+++ Algorithm/src/leet/Q20260723.java
@@ -1,4 +1,28 @@
 package leet;
 
+import java.util.Arrays;
+
 public class Q20260723 {
+
+    public int singleNumber(int[] nums) {
+        int n = nums.length;
+        Arrays.sort(nums);
+        int result = nums[0];
+        int count = 1;
+
+        for (int i = 1; i < n; i++) {
+            if (result == nums[i]) {
+                count++;
+            } else {
+                if (count == 2) {
+                    count = 1;
+                    result = nums[i];
+                } else {
+                    break;
+                }
+            }
+        }
+
+        return result;
+    }
 }
