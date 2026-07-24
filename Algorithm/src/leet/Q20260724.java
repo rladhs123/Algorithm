117073e8ae3de10b8d300e2b0a00475be0b8d63a
rladhs123 <151420186+rladhs123@users.noreply.github.com>
2026-07-24 13:53:56 +0900

    day 480

--- Algorithm/src/leet/Q20260724.java
+++ Algorithm/src/leet/Q20260724.java
@@ -1,4 +1,23 @@
 package leet;
 
 public class Q20260724 {
+
+    public int missingNumber(int[] nums) {
+        int n = nums.length;
+        boolean[] check = new boolean[n + 1];
+        int result = 0;
+
+        for (int i : nums) {
+            check[i] = true;
+        }
+
+        for (int i = 0; i < n + 1; i++) {
+            if (!check[i]) {
+                result = i;
+                break;
+            }
+        }
+
+        return result;
+    }
 }
