caf5f402c1c2dcb99450660d64e51a8db5feb806
rladhs123 <151420186+rladhs123@users.noreply.github.com>
2026-09-03 08:31:34 +0900

    day 521

---
+++ Algorithm/src/leet/Q20260903.java
@@ -0,0 +1,14 @@
+package leet;
+
+public class Q20260903 {
+
+    public String toLowerCase(String s) {
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < s.length(); i++) {
+            sb.append(Character.toLowerCase(s.charAt(i)));
+        }
+
+        return sb.toString();
+    }
+}
