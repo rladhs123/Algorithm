0c4bf3a893af3ada506df6ed86d42278a2ef190e
rladhs123 <151420186+rladhs123@users.noreply.github.com>
2026-09-05 18:15:46 +0900

    day 523

---
+++ Algorithm/src/leet/Q20260905.java
@@ -0,0 +1,27 @@
+package leet;
+
+public class Q20260905 {
+
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public boolean checkTree(TreeNode root) {
+        return root.val == (root.left.val + root.right.val);
+    }
+}
