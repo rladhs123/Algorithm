d7c137996606fc1cb76419aa3e35e621bd0ba53e
rladhs123 <151420186+rladhs123@users.noreply.github.com>
2026-09-08 15:35:13 +0900

    day 526

---
+++ Algorithm/src/leet/Q20260908.java
@@ -0,0 +1,37 @@
+package leet;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Q20260908 {
+
+    public boolean isAnagram(String s, String t) {
+        Map<Character, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+
+        for (int i = 0; i < t.length(); i++) {
+            char c = t.charAt(i);
+
+            if (!map.containsKey(c)) {
+                return false;
+            }
+
+            map.put(c, map.get(c) - 1);
+        }
+
+        boolean result = true;
+
+        for (char c : map.keySet()) {
+            if (map.get(c) != 0) {
+                result = false;
+                break;
+            }
+        }
+
+        return result;
+    }
+}
