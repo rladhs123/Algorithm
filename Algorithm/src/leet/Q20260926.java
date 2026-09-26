51e5137b7019c930806a794e5a13bd5bacaa39f7
rladhs123 <151420186+rladhs123@users.noreply.github.com>
2026-09-26 16:26:47 +0900

    day 544

---
+++ Algorithm/src/leet/Q20260926.java
@@ -0,0 +1,16 @@
+package leet;
+
+public class Q20260926 {
+
+    public int commonFactors(int a, int b) {
+        int result = 0;
+
+        for (int i = 1; i <= Math.min(a, b); i++) {
+            if (a % i == 0 && b % i == 0) {
+                result++;
+            }
+        }
+
+        return result;
+    }
+}
